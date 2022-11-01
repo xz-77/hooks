1af8be35b66def82ffdd61dbf539cbcad730385e
jinzeze <jinzeze@medlinker.com>
2022-11-01 16:02:47 +0800

    refactor: 更新

--- src/useEvent/index.ts
+++ src/useEvent/index.ts
@@ -1,8 +1,7 @@
-import  { useCallback, useLayoutEffect, useRef } from 'react';
+import { useCallback, useLayoutEffect, useRef } from 'react';
 
-
-function useEvent(handler) {
-  const handlerRef = useRef(null);
+function useEvent(handler: any) {
+  const handlerRef = useRef<any>(null);
 
   // 视图渲染完成后更新`handlerRef.current`指向
   useLayoutEffect(() => {
@@ -11,8 +10,8 @@ function useEvent(handler) {
 
   // 用useCallback包裹，使得render时返回的函数引用一致
   return useCallback((...args) => {
-    const fn = handlerRef.current as any;
-    return fn(...args);
+    const fn = handlerRef.current;
+    return fn?.(...args);
   }, []);
 }
 
